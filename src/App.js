049f44e67281978f00b75b48408d2eae449295e5
gnasherx <pawarg124@gmail.com>
2018-11-20 16:14:19 +0530

    New Slack-React repo is all set now

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <h2>Repo setup</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
